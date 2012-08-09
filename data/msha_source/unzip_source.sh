dfd974bb91ab65a8e509af024f4751d3964a3137
Matt Frost <mwfrost@gmail.com>
2012-08-08 22:06:38 -0400

    Can't recall what these changes entailed.

---
+++ data/msha_source/unzip_source.sh
@@ -0,0 +1,8 @@
+unzip -o Accidents.zip
+unzip -o AddressOfRecord.zip
+unzip -o Inspections.zip
+unzip -o Mines.zip
+unzip -o MinesProdQuarterly.zip
+unzip -o MinesProdYearly.zip
+unzip -o Violations.zip
+unzip -o Mines.zip
