3a4d3d5866a9567d34113b6cdd1efc13c4ab1776
Matt Frost <matthew_frost@sra.com>
2012-11-21 22:30:17 -0500

    Added calendarHeat functionality; calculated time since last event

--- src/msha.r
+++ src/msha.r
@@ -20,6 +20,117 @@ vdat <- read.csv("./data/wv_vdat.csv")
 vdat <- merge(vdat, mdat_wv[,c('MINE_ID','CURRENT_MINE_NAME','CURRENT_CONTROLLER_NAME')])
 
 
+# Reduce mdat_wv to only mines that are not abandoned
+mdat_wv <- mdat_wv[grep('Abandoned.*',mdat_wv$CURRENT_MINE_STATUS, invert=TRUE),]
+
+#reduce inspection and violation tables accordingly
+idat <- subset(idat, MINE_ID %in% mdat_wv$MINE_ID)
+vdat <- subset(vdat, MINE_ID %in% mdat_wv$MINE_ID)
+
+
+# Create a time series by mine and month
+
+# add real dates to the various data.frames
+mdat_wv$current.status.dt <- mdy(mdat_wv$CURRENT_STATUS_DT)
+idat$inspection.begin.dt <- mdy(idat$INSPECTION_BEGIN_DT)
+vdat$violation.occur.dt <- mdy(vdat$VIOLATION_OCCUR_DT)
+adat$accident.dt <- mdy(adat$ACCIDENT_DT)
+
+names(adat)
+names(idat)
+names(vdat)
+names(mdat)
+
+
+
+# Count all violations by date
+vxday <- ddply(vdat, .(violation.occur.dt), nrow)
+names(vxday) <- c('date','vcount')
+calendarHeat(subset(vxday, year(date)==2011)$date , subset(vxday, year(date)==2011)$vcount)
+
+# Count, for every day, the days since an inspection, the days since a violation, and the days since an accident
+# Eaxmple mine: Robinson Run 4601318
+
+idays <- data.frame(
+                MINE_ID = 4601318,
+                day=seq(
+                from=min(subset(idat, MINE_ID == 4601318)$inspection.begin.dt),
+                to=max(subset(idat, MINE_ID == 4601318)$inspection.begin.dt),
+                by=86400
+                )
+              )
+
+idays <- merge(idays, subset(idat, MINE_ID==4601318)[,c('MINE_ID','inspection.begin.dt', 'EVENT_NO')], 
+    by.x=c('MINE_ID','day'),
+    by.y=c('MINE_ID', 'inspection.begin.dt'),
+    all.x=TRUE)
+
+idays$idate <- ifelse(is.na(idays$EVENT_NO),0,decimal_date(idays$day))
+
+idays$yrssince <- decimal_date(idays$day) - cummax(ifelse(!is.na(idays$EVENT_NO),decimal_date(idays$day),0)) 
+calendarHeat(
+  subset(idays, year(day)==2011)$day , 
+  subset(idays, year(day)==2011)$yrssince  
+  )
+
+
+
+
+# Collapse inspections, violations, and accidents into "event" spans
+
+# Summarize inspections by MINE_ID
+mine.isum <- ddply(idat, .(MINE_ID), summarize, event='inspection', first.event=min(inspection.begin.dt), last.event=max(inspection.begin.dt), event.span=max(inspection.begin.dt)-min(inspection.begin.dt), event.count=length(inspection.begin.dt))
+
+# Summarize violations by MINE_ID
+mine.vsum <- ddply(vdat, .(MINE_ID), summarize, event='violation', first.event=min(violation.occur.dt), last.event=max(violation.occur.dt), event.span=max(violation.occur.dt)-min(violation.occur.dt), event.count=length(violation.occur.dt))
+
+# Summarize accidents by MINE_ID
+mine.asum <- ddply(adat, .(MINE_ID), summarize, event='accident',first.event=min(accident.dt), last.event=max(accident.dt), event.span=max(accident.dt)-min(accident.dt), event.count=length(accident.dt))
+
+# Combine all the summaries into an events table
+mine.sum <- rbind(mine.vsum,mine.asum,mine.isum)
+
+# Scatterplot of event span vs event count
+ggplot(mine.sum) + geom_point(aes(color=event,x=as.numeric(event.span),y=event.count))
+
+# Line plot of event spans
+ggplot(mine.sum) +geom_segment(aes(color=event,x=first.event,y=event.count,xend=last.event,yend=event.count))
+
+
+
+
+
+
+# Build a time series of days since each event, with zero for days when events happened
+
+# example: mine 4601318
+subset(
+ddply(mine.sum, .(MINE_ID), summarise, first.entry=min(first.event), last.entry=max(last.event))
+, MINE_ID == 4601318
+)
+
+all.days <- seq(from=min(mine.sum$first.event),to=max(mine.sum$last.event),by=86400)
+
+# Date sequence example:
+#  seq(from=ymd("2000-01-04"), to=ymd("2000-01-07"), by=86400)
+
+vdays <- ddply(head(arrange(mine.vsum, desc(event.count)),7), .(MINE_ID), function(x)
+data.frame(
+	MINE_ID=x$MINE_ID[1]
+	, date=seq(from=min(x$first.event),to=max(x$last.event),by=86400)
+	)
+)
+
+vdays <- ddply(vdays, .(MINE_ID, date) , function(x) c(last.vi.date=max(subset(vdat, MINE_ID==x$MINE_ID & violation.occur.dt <= x$date)$violation.occur.dt)
+	)
+)
+
+
+
+
+
+
+
 #  mine and operator violation counts
 
 ddply(head(vdat,500), c('VIOLATOR_NAME','MINE_NAME'),summarise, n_violator=length(VIOLATOR_NAME), n_mine=length(MINE_NAME))
