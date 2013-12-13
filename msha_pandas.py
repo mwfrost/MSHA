164b4a72b2e1741cbf249df0801c8313ea5dcf40
Matt Frost <mwfrost@gmail.com>
2013-12-13 16:19:51 -0500

    Built logit model in pandas after the data load wiped out R.

--- msha_pandas.py
+++ msha_pandas.py
@@ -1,12 +1,16 @@
 
-
+#%%
 import pandas as pd
 import numpy as np
 import dateutil
 import os
+import statsmodels.api as sm
+import lifelines
+
 
 os.chdir('/Users/mfrost/Code/MSHA')
 
+#%%
 mines = pd.read_table('data/msha_source/Mines.txt', sep='|' , parse_dates=['CURRENT_CONTROLLER_BEGIN_DT'])
 mines_index = mines.set_index(['STATE','MINE_ID','CURRENT_MINE_STATUS','CURRENT_STATUS_DT','CURRENT_CONTROLLER_ID','CURRENT_CONTROLLER_NAME','CURRENT_OPERATOR_ID','CURRENT_OPERATOR_NAME'])
 
@@ -24,6 +28,8 @@ violations['VIOLATION_OCCUR_DT']=violations['VIOLATION_OCCUR_DT'].apply(dateutil
 
 
 # Full Data Loading Complete
+
+#%%
 
 # Filter to just a state of interest
 sub_state = 46
@@ -43,6 +49,7 @@ accidents.to_csv('data/wv_accidents.csv',index='FALSE')
 
 # Filtering complete
 
+#%%
 # Aggregation tasks:
 # - build a list of operating days for each mine
 # - given a mine, a date, and a trailing window, return the number of violations, 
@@ -74,6 +81,8 @@ i_days.tail(10)
 # Use the .xs() function to select a cross-section
 i_days.xs(4601318).tail(20)
 
+
+#%%
 # Accidents
 def a_counter(grouped):
     se = grouped.set_index('ACCIDENT_DT')['DEGREE_INJURY_CD']
@@ -93,7 +102,7 @@ a_days.tail(10)
 
 
 
-
+#%%
 
 
 # Violations
@@ -106,16 +115,34 @@ v_test=violations[:][violations['MINE_ID'].isin([4601318, 94601456])]
 
 
 grouped = violations.groupby(['MINE_ID','VIOLATION_OCCUR_DT'])
-v_days=grouped['VIOLATION_NO'].agg({'vcount':np.size})
-v_days.reset_index(level=[0], inplace=True)
+
+v_days = grouped['VIOLATION_NO'].agg({'vcount':np.size})
+
+# Having trouble resampling the days when the date is in the MultiIndex
+# http://stackoverflow.com/questions/15799162/resampling-within-a-pandas-multiindex
+
+v_days.reset_index(level=[0,1], inplace=True)
+v_days = pd.DataFrame(v_days)
+v_days.head()
+v_days['VIOLATION_OCCUR_DT'] = pd.to_datetime(v_days['VIOLATION_OCCUR_DT'])
+v_days.head()
+v_days = v_days.sort(['MINE_ID','VIOLATION_OCCUR_DT'])
+v_days.head()
+
+
+v_days.set_index(pd.DatetimeIndex(v_days['VIOLATION_OCCUR_DT']), inplace=True)
 
 # Resample and fill non-violation days
 def _sum(x):
     if len(x) == 0: return 0
     else: return sum(x)
+v_days.resample('D',how=_sum)
 
+del v_days['VIOLATION_OCCUR_DT']
+v_days.groupby('MINE_ID').resample('D',how=_sum).head()
 v_days = v_days.groupby(['MINE_ID']).resample('D',how=_sum)
 del v_days['MINE_ID']
+
 
 v30 = v_days.apply(lambda x: pd.rolling_sum(x,30,min_periods=1))
 v30.columns = ['v30']
@@ -126,6 +153,8 @@ v_days['v30'] = v30.v30
 v_days['v90'] = v90.v90
 
 v_days.head(30)
+
+#%%
 
 # Merge accidents to inspections and violations
 
@@ -139,9 +168,47 @@ iav_days = pd.merge(ia_days, v_days, how='left', left_index=True, right_index=Tr
 # with the entire US data set, it processed 63,252,285 records in between 3 and 5 hours
 
 
+iav_days['has_acc'] = iav_days['degree_injury_cd'] >= 0
+iav_days[iav_days['has_acc'] == 1].head()
+
+
+#%%
+
 iav_days.to_csv('data/iav_days.csv',index='FALSE')
 
-# TODO: Convert objects to R
+#%%
+op_days = iav_days.reset_index(level=[0])
+submines = mines[['MINE_ID','COAL_METAL_IND','CURRENT_MINE_TYPE','CURRENT_CONTROLLER_ID','CURRENT_OPERATOR_ID']]
+op_days = pd.merge(op_days, submines, on='MINE_ID')
+
+#%% Logit
+
+# Drop the na values because logit can't handle them
+op30 = op_days[['i30','v30','a30','has_acc']].dropna(axis=0)
+
+logit = sm.Logit(op30['has_acc'], op30[['v30','i30','a30']])
+
+result = logit.fit()
+
+print result.summary()
+
+# look at the confidence interval of each coeffecient
+print result.conf_int()
+# odds ratios
+print np.exp(result.params)
+params = result.params
+conf = result.conf_int()
+conf['OR'] = params
+conf.columns = ['2.5%', '97.5%', 'OR']
+print np.exp(conf)
+
+# Trailing inspections and violations inside an arbitrary window doesn't result in a very cool model. 
+# Survival analysis might be better.
+
+# TODO: convert the data toright-censored durations and use the lifelines
+# library: http://nbviewer.ipython.org/github/CamDavidsonPilon/lifelines/blob/master/Tutorial%20and%20Examples.ipynb
+
+
 
 
 
